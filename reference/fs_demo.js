360cea460c2c89592da90049cb3b3ece12ab7300
SeyoungKim <ksy4961@gmail.com>
2019-05-07 22:52:19 +1000

    Rename, read, create and write files and folders

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,7 +2,30 @@ const fs = require('fs');
 const path = require('path');
 
 // create folder
-fs.mkdir(path.join(__dirname, '/test'), {}, function (err) {
+// fs.mkdir(path.join(__dirname, '/test'), {}, function (err) {
+//     if (err) throw err;
+//     console.log('Folder create...');
+// });
+
+// Create and write to file 
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', function (err) {
+//     if (err) throw err;
+//     console.log('File written to ...');
+//     // File append
+//     fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), ' I Love nodejs', function (err) {
+//         if (err) throw err;
+//         console.log('File written to ...');
+//     });
+// });
+
+// Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', function (err, data) {
     if (err) throw err;
-    console.log('Folder create...');
+    console.log(data);
+});
+
+// Rename file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), function (err) {
+    if (err) throw err;
+    console.log('File renamed...');
 });
