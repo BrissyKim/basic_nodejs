e2bb1f7ff8c8199a10dcec79ce624b41457e387e
SeyoungKim <ksy4961@gmail.com>
2019-05-07 23:15:36 +1000

    event demo

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const person = require('./person');
-console.log(person.name);
+// const person = require('./person');
+// console.log(person.name);
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', function (data) {
+    console.log('Called Listener', data);
+});
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
